f1a627afa7de64e6f6681d2fd593ed3a074c4e9f
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2017-08-23 13:49:24 +0500

    Refactoring: small code cleanup

--- tcp-stream-splitter.lua
+++ tcp-stream-splitter.lua
@@ -1,13 +1,13 @@
 do
     local args = { ... }
-    assert(args[1], "You should pass -X lua_script1:$PATH_TO_SOURCE_PCAP_FILE")
+    local pcap_file = args[1]
+    assert(pcap_file, "You should pass -X lua_script1:$PATH_TO_SOURCE_PCAP_FILE")
     local streams_table = {}
     local tcp_stream_f = Field.new("tcp.stream")
 
     local function init_listener()
-        filter = ""
-        local tap = Listener.new("frame", filter)
-
+        local tap = Listener.new("frame", "")
+        os.execute(string.format("mkdir -p %s.parts/", pcap_file))
         function tap.reset()
             for _, value in pairs(streams_table) do
                 value.dumper:flush()
@@ -15,17 +15,15 @@ do
             end
         end
 
-        function tap.packet(pinfo)
+        function tap.packet()
             local tcp_stream = assert(tonumber(tostring(tcp_stream_f())))
             local index = tcp_stream + 1 -- in Lua arrays starts with 1 (and not with 0)
-
+            local part_pcap
             if streams_table[index] == nil then
-                streams_table[index] = {
-                    tcp_stream = tcp_stream,
-                    dumper = Dumper.new_for_current(string.format("%s.parts/%d.pcap", args[1], index))
-                }
+                part_pcap = string.format("%s.parts/%d.pcap", pcap_file, index)
+                streams_table[index] = Dumper.new_for_current(part_pcap)
             end
-            streams_table[index].dumper:dump_current()
+            streams_table[index]:dump_current()
         end
     end
 
