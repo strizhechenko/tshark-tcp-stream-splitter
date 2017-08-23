870d797957677522b5fc46b2cbf7291a3ae35619
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2017-08-23 12:15:34 +0500

    Create tcp-stream-splitter.lua

---
+++ tcp-stream-splitter.lua
@@ -0,0 +1,31 @@
+do
+    local streams_table = {}
+    local tcp_stream_f = Field.new("tcp.stream")
+
+    local function init_listener()
+        filter = ""
+        local tap = Listener.new("frame", filter)
+
+        function tap.reset()
+            for _, value in pairs(streams_table) do
+                value.dumper:flush()
+                value.dumper:close()
+            end
+        end
+
+        function tap.packet(pinfo)
+            local tcp_stream = assert(tonumber(tostring(tcp_stream_f())))
+            local index = tcp_stream + 1 -- in Lua arrays starts with 1 (and not with 0)
+
+            if streams_table[index] == nil then
+                streams_table[index] = {
+                    tcp_stream = tcp_stream,
+                    dumper = Dumper.new_for_current(string.format("revisor.pcap.parts/%d.pcap", index))
+                }
+            end
+            streams_table[index].dumper:dump_current()
+        end
+    end
+
+    init_listener()
+end
